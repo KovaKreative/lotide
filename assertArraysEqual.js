c006ae051558d830074d5dadb78b2e7008a9b961
KovaKreative <eric.kovalevskyy@gmail.com>
2023-03-07 18:47:45 -0500

    implemented assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(arrayA, arrayB) {
+  if(arrayA.length === arrayB.length) {
+    for (let i = 0; i < arrayA.length; i++) {
+      if(arrayA[i] !== arrayB[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  return false;
+};
+
+const assertArraysEqual = function(arrayA, arrayB) {
+
+  let output = eqArrays(arrayA, arrayB) ? `✅Assertion Passed: [${arrayA}] === [${arrayB}]` : `⛔Assertion Failed: [${arrayA}] !== [${arrayB}]`;
+
+  console.log(output);
+};
